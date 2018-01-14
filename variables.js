da8d3a038c7dcb183d9b8bb53ab14dab4236f5e5
Lana Worobec <llama.p0rtk3y@gmail.com>
2018-01-14 06:26:13 +0000

    automatically backed up by learn

--- variables.js
+++ variables.js
@@ -1,12 +1,14 @@
 name = "Beyonce";
 console.log(name);
 
-var personality = "awesome"
+var personality = "bumbling"
 console.log(personality)
 //
 
 personality = "fabulous";
 console.log(personality);
+
+var totalSum = 500 + 750 + 3000 + 5100 + 12980 + 9087 + 50000
 
 // Your Turn!
 // change the value of the `personality` variable to something that describes you
